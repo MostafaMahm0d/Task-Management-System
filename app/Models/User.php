57124e6c30c1cf92a51bc05e9ca880f6fba4fa8f
alaa.habib <alaa.habib@objects.email>
2026-08-31 19:23:00 +0300

    Add filament user in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use App\Enums\TaskPriority;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -20,10 +22,15 @@ use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password', 'email_verified_at'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable implements Commentator, MustVerifyEmail
+class User extends Authenticatable implements Commentator, FilamentUser, MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use CanComment, HasApiTokens, HasFactory, HasRoles, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * Get the attributes that should be cast.
